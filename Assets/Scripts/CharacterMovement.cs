54c84225ff11587906e10c2b07f8b52f2e8f30b3
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-05-12 10:52:03 +0200

    Updated 1

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+	public static int movespeed = 1;
+	public Vector3 userDirection = Vector3.right;
+
+	public void Update()
+	{
+		transform.Translate(userDirection * movespeed * Time.deltaTime); 
+	}
+}
