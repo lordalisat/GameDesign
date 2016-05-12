49356c949966304ca4b4f0e5e63883b6c46c25c3
lordalisat <b.wieringa31@gmail.com>
2016-05-12 15:21:58 +0200

    Sighting fixed

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySight : MonoBehaviour
+{
+	public float fieldOfViewAngle = 60f;           // Number of degrees, centred on forward, for the enemy see.
+	public bool playerSighted;                      // Whether or not the player is currently sighted.
+	public Sighting sighting;
+
+	private SphereCollider col;                     // Reference to the sphere collider trigger component.
+	private Animator anim;                          // Reference to the Animator.
+	private GameObject player;                      // Reference to the player.
+	private Animator playerAnim;                    // Reference to the player's animator component.
+	private GameObject controller;
+	private bool caught = false;
+
+	void Awake ()
+	{
+		// Setting up the references.
+		col = GetComponent<SphereCollider>();
+		anim = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerAnim = player.GetComponent<Animator>();
+		controller = GameObject.FindGameObjectWithTag ("GameController");
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		// If the player has entered the trigger sphere...
+		if (other.gameObject == player) {
+
+			// Create a vector from the enemy to the player and store the angle between it and forward.
+			Vector3 direction = other.transform.position - transform.position;
+			float angle = Vector3.Angle (direction, transform.forward);
+
+			// If the angle between forward and where the player is, is less than half the angle of view...
+			if (angle < fieldOfViewAngle * 0.5f) {
+				RaycastHit hit;
+
+				// ... and if a raycast towards the player hits something...
+				if (Physics.Raycast (transform.position + transform.up, direction.normalized, out hit, col.radius)) {
+					// ... and if the raycast hits the player...
+					if (hit.collider.gameObject == player) {
+						if (!caught) {
+							caught = true;
+							// ... the player is in sight.
+							sighting.playerSighted ();
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Sighting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sighting : MonoBehaviour {
+
+	AudioSource audio;
+	public AudioClip MetalGear;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}	
+
+	public void playerSighted () {
+		audio.Stop ();
+		audio.clip = MetalGear;
+		audio.PlayOneShot (MetalGear, 0.5f);
+		Debug.Log ("nope");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+}
