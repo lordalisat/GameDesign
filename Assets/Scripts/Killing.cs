954f1f5255b94c6962f2542f78dd23d43dc5e698
R.B.T. Bens <r.b.t.bens@student.tue.nl>
2016-06-06 01:13:35 +0200

    Fixed killing, semi ragdolls for enemies and added a knife

--- Assets/Scripts/Killing.cs
+++ Assets/Scripts/Killing.cs
@@ -4,7 +4,7 @@ using System;
 
 public class Killing : MonoBehaviour
 {
-	public KillCollision collider;
+	public KillCollision enemyCollider;
 	public Animation anim;
 	public Animator baseAnim;
 
@@ -19,18 +19,20 @@ public class Killing : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (!collider.CanKill())
-			return;
-
-		if (DateTime.Now > animEnd)
-		{
-			if (!baseAnim.enabled)
-			{
+		// Only check when we're not in animation
+		if (DateTime.Now > animEnd) {
+			// This indicates we're running our own animation and that it's done
+			if (!baseAnim.enabled) {
 				baseAnim.enabled = true;
-			}
-			else if (Input.GetKeyDown(KeyCode.Space))
-			{
-				Destroy(collider.GetTarget());
+
+				// Only kill the enemy when we're STILL behind him
+				if (enemyCollider.CanKill()) {
+					//Destroy(enemyCollider.GetTarget());
+					enemyCollider.GetTarget().transform.Rotate(new Vector3(-90, 0, 0));
+				}
+
+			// Check if we're behind the enemy and pressing space
+			} else if (Input.GetKeyDown(KeyCode.Space) && enemyCollider.CanKill()) {
 				baseAnim.enabled = false;
 
 				anim = GetComponentInChildren<Animation>();
