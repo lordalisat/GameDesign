efec53bd03df4810917656070894df19be35f1ee
R.B.T. Bens <r.b.t.bens@student.tue.nl>
2016-06-06 22:08:33 +0200

    Waypoints are now coupled in sets, enemy path following goes smoother and allowed waypoints to higher amounts

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -3,40 +3,50 @@ using System.Collections;
 
 public class EnemyPath : MonoBehaviour
 {
-
-	public GameObject[] wayPoints;
-	public int num = 0;
-
-	private GameObject player;
-
+	public GameObject waypointSet;
 	public float minDist = 1;
 	public float speed = 1;
 	public float rSpeed = 2;
+	public bool killed = false;
 
+	private GameObject player;
+	private Transform[] waypoints;
+
+	private int num = 0;
 	private bool rand = false;
 	private bool go = true;
-	public bool killed = false;
 
 	// Use this for initialization
 	void Start()
 	{
+		// Get the player
 		player = GameObject.FindGameObjectWithTag("Player");
+
+		// Get the amount of children on the set
+		waypoints = new Transform[waypointSet.transform.childCount];
+
+		// Add the waypoints to the array
+		for (int i = 0; i < waypoints.Length; i++)
+			waypoints [i] = waypointSet.transform.GetChild (i).transform;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		// Don't update if we're dead, yeah...
 		if (killed)
 			return;
-		
-		float dist = Vector3.Distance(gameObject.transform.position, wayPoints[num].transform.position);
 
+		// Get the distance to the current waypoint
+		float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].position);
+
+		// Check if we can go
 		if (go) {
 			if (dist > minDist) {
 				Move();
 			} else {
 				if (!rand) {
-					if (num + 1 == wayPoints.Length) {
+					if (num + 1 == waypoints.Length) {
 						num = 0;
 					} else {
 						num++;
@@ -52,12 +62,14 @@ public class EnemyPath : MonoBehaviour
 
 	public void Move()
 	{
-		Quaternion direction = Quaternion.LookRotation(wayPoints[num].transform.position - gameObject.transform.position);
+		Quaternion direction = Quaternion.LookRotation(waypoints[num].position - gameObject.transform.position);
 		gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, direction, rSpeed * Time.deltaTime);
-		if (Vector3.Angle(wayPoints[num].transform.position - gameObject.transform.position, gameObject.transform.forward) < 40) {
+
+		if (Vector3.Angle(waypoints[num].position - gameObject.transform.position, gameObject.transform.forward) < 40) {
 			gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
-		} else if (Vector3.Angle(wayPoints[num].transform.position - gameObject.transform.position, gameObject.transform.forward) < 130) {
-			gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime / 30;
+		} else if (Vector3.Angle(waypoints[num].position - gameObject.transform.position, gameObject.transform.forward) < 130) {
+			gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+			gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, direction, rSpeed * Time.deltaTime * 15);
 		} else {
 			gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, direction, rSpeed * Time.deltaTime);
 		}
@@ -67,6 +79,7 @@ public class EnemyPath : MonoBehaviour
 	{
 		Quaternion direction = Quaternion.LookRotation(player.transform.position - gameObject.transform.position);
 		gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, direction, rSpeed * Time.deltaTime * 5);
+
 		float dist = Vector3.Distance(gameObject.transform.position, player.transform.position);
 		if (dist > 2 && gameObject.transform.rotation == direction) {
 			gameObject.transform.position += gameObject.transform.forward * speed * 2 * Time.deltaTime;
@@ -77,12 +90,13 @@ public class EnemyPath : MonoBehaviour
 
 	void RandomWayPoint()
 	{
-		num = Random.Range(0, wayPoints.Length);
-		Vector3 direction = wayPoints[num].transform.position - gameObject.transform.position;
+		num = Random.Range(0, waypoints.Length);
+		Vector3 direction = waypoints[num].position - gameObject.transform.position;
 		RaycastHit hit;
+
 		if (Physics.Raycast(transform.position + transform.up, direction.normalized, out hit)) {
 			// ... and if the raycast hits the player...
-			if (!(hit.collider.gameObject == wayPoints[num])) {
+			if (!(hit.collider.gameObject == waypoints[num].gameObject)) {
 				RandomWayPoint();
 			}
 		}
